9c9672dc45235cbc22ca191432aa56ccc34e7b3f
Breania Harvey <harvey.breania@gmail.com>
2022-01-01 15:46:07 -0600

    thought list component

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { 
+  ApolloProvider, 
+  ApolloClient, 
+  InMemoryCache, 
+  createHttpLink 
+} from '@apollo/client';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+
+// new link established to GraphQL server
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql'
+});
+
+// ApolloClient() constructor to instantiate the Apollo Client & create the connection to the API endpoint
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className='flex-column justify-flex-start min-100-vh'>
+        <Header />
+        <div className='container'>
+          <Home />
+        </div>
+        <Footer />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
